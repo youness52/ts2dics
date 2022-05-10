73cf8505198b3876f84e44340975839981a25e8a
anayouness52@gmail.com <anayouness52@gmail.com>
2022-05-10 20:50:09 +0100

    000

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 require('config.php');
 $sql = "INSERT INTO `etudiants` (`id`, `fname`, `lname`, `email`, `tele`) VALUES (NULL, 'samasi', 'karim', 'email@gmail.com', '065654565646')";
 mysqli_query($db,$sql) or die("Erreur query");
-echo "Etudiant est bien enregistrer";
+echo "Etudiant est bien enregistrer ?";
 mysqli_close($db);
 
 ?>
