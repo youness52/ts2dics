d98c96ebd1fe33dd7e5fa3f7e3d7d638a901ed10
anayouness52@gmail.com <anayouness52@gmail.com>
2022-05-17 21:08:44 +0100

    546

--- PDO/insert.php
+++ PDO/insert.php
@@ -1,7 +1,8 @@
 <?php
-$vr="hello";
+
 require('config.php');
-$sql=$conn->prepare("INSERT INTO `etudiants` (`id`, `fname`, `lname`, `email`, `tele`) VALUES (NULL, '$vr', 'karim', 'email@gmail.com', '065654565646')");
+$sql=$conn->prepare("INSERT INTO `etudiants` (`id`, `fname`, `lname`, `email`, `tele`) 
+VALUES (NULL, '1245', 'karim', 'email@gmail.com', '065654565646')");
 $sql->execute();
 echo "Etudiant est bien enregistrer ?";
 $sql=null;
